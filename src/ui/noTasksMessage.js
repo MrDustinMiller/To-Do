20c0e7f254955cf044c2c2a63a35f5022dfd86fd
MrDustinMiller <dustin.miller@mscoding.org>
2023-07-14 01:18:21 -0500

    move code to own module

---
+++ src/ui/noTasksMessage.js
@@ -0,0 +1,16 @@
+export function makeNewTaskDiv() {
+  const mainRightBody = document.querySelector('.main-right');
+
+  const noTaskDiv = document.createElement('div');
+  noTaskDiv.classList.add('no-task-container');
+  mainRightBody.appendChild(noTaskDiv);
+
+  const noTaskText = document.createElement('p');
+  noTaskText.textContent = 'You have no tasks yet, lets make some!';
+  noTaskDiv.appendChild(noTaskText);
+
+  const newTaskButton = document.createElement('i');
+  newTaskButton.setAttribute('class', 'main-new fa-regular fa-plus fa-2xl');
+  newTaskButton.setAttribute('title', 'New');
+  noTaskDiv.appendChild(newTaskButton);
+}
