4ef1eab1847c2d48ed4111395226518cad2f020c
MrDustinMiller <dustin.miller@mscoding.org>
2023-07-27 20:55:22 -0500

    add dialog modal events

--- src/pages/taskList.js
+++ src/pages/taskList.js
@@ -1,10 +1,12 @@
-import { makeNewTaskDiv } from '../ui/noTasksMessage';
+import makeNewTaskDiv from '../ui/noTasksMessage';
 import createHeader from '../ui/header';
+import { dialogEvents } from '../events';
 
 export default function loadTaskList() {
   createHeader();
   //if no tasks in localstorage show
   makeNewTaskDiv();
+  dialogEvents();
   // else
   // show tasks in local storage
   // probably make LS its own module and import it here to use
