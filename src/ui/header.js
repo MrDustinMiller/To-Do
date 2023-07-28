3de7712398ba17a4ac64b404f3fb322eee95f192
MrDustinMiller <dustin.miller@mscoding.org>
2023-07-27 20:57:01 -0500

    delete unused attribute code

--- src/ui/header.js
+++ src/ui/header.js
@@ -2,10 +2,6 @@ export default function createHeader() {
   const sidebarLinkClicked = document.querySelector('.active').innerHTML;
   const mainBody = document.querySelector('.main-right');
 
-  const mainHeader = document.createElement('div');
-  mainHeader.classList.add('main-header');
-  mainBody.appendChild(mainHeader);
-
   const pageHeader = document.createElement('div');
   pageHeader.classList.add('page-header');
   pageHeader.innerHTML = `${sidebarLinkClicked}`;
