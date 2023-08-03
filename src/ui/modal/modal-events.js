ba727669f0ea26d83d56f55ff3c48dac62a65304
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-03 18:19:20 -0500

    add events to load each page and closing modal

--- src/ui/modal/modal-events.js
+++ src/ui/modal/modal-events.js
@@ -2,27 +2,41 @@ import {
   addActiveAttribute,
   checkForActiveAttribute,
 } from '../activeAttribute';
+import loadModalTaskPage from './modal-task-page';
+import loadModalProjectPage from './modal-project-page';
+import loadModalNotesPage from './modal-note-page';
 
-export default function modalEvents() {
+export default function modalEvents(newModal, container) {
   const modalSidebarLinks = document.querySelectorAll('.modal-link');
+  const modalCloseBtn = document.querySelector('.modal-close-btn');
 
   modalSidebarLinks.forEach((link) => {
     link.addEventListener('click', (e) => {
       checkForActiveAttribute(modalSidebarLinks);
       addActiveAttribute(e);
-      checkWichModalSidebarLinkIsClicked(e.target.textContent);
+      checkWichModalSidebarLinkIsClicked(e);
     });
+  });
+
+  newModal.addEventListener('close', () => {
+    container.removeChild(newModal);
+  });
+
+  modalCloseBtn.addEventListener('click', () => {
+    container.removeChild(newModal);
   });
 }
 
 function checkWichModalSidebarLinkIsClicked(link) {
-  // will need to load each modal page here by importing
-  switch (link) {
+  switch (link.target.textContent) {
     case 'Task':
+      loadModalTaskPage();
       break;
     case 'Project':
+      loadModalProjectPage();
       break;
     case 'Note':
+      loadModalNotesPage();
       break;
 
     default:
