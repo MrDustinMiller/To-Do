e1e8d81a2cacd6555b46af5823d4da390b2912bc
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-03 18:21:38 -0500

    add function to wipe modal content

--- src/ui/dom.js
+++ src/ui/dom.js
@@ -1,7 +1,15 @@
 export function wipeDomContent() {
   const content = document.querySelector('.main-right');
+
   if (content.hasChildNodes()) {
     // if mainContent has a child, replace w/empty string so we can show next nav items clicked content
+    content.textContent = '';
+  }
+}
+
+export function wipeModalContent(content) {
+  if (content.hasChildNodes()) {
+    // if modal main has a child, replace w/empty string so we can show next nav items clicked content
     content.textContent = '';
   }
 }
