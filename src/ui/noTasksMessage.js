93ca06f2414b3a77ba7af1b0709268e77e6fda25
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-03 18:21:09 -0500

    refactored code, broke into smaller functions

--- src/ui/noTasksMessage.js
+++ src/ui/noTasksMessage.js
@@ -1,14 +1,20 @@
-export default function makeNewTaskDiv() {
+export default function makeNewTaskDiv(choice) {
   const mainRightBody = document.querySelector('.main-right');
-
   const noTaskDiv = document.createElement('div');
   noTaskDiv.classList.add('task-container');
   mainRightBody.appendChild(noTaskDiv);
 
-  const noTaskText = document.createElement('p');
-  noTaskText.textContent = 'You have no tasks yet, lets make some!';
-  noTaskDiv.appendChild(noTaskText);
+  createNoTaskContainerText(noTaskDiv, choice);
+  createNoTaskContainerButton(noTaskDiv);
+}
 
+function createNoTaskContainerText(noTaskDiv, choice) {
+  const noTaskText = document.createElement('p');
+  noTaskText.textContent = `You have no ${choice} yet, lets make some!`;
+  noTaskDiv.appendChild(noTaskText);
+}
+
+function createNoTaskContainerButton(noTaskDiv) {
   const newTaskButton = document.createElement('i');
   newTaskButton.setAttribute(
     'class',
