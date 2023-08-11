276495ac84244d98ca27229ada6db7d115dcffa3
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-10 22:51:45 -0500

    reworking project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,28 @@
 import createHeader from '../ui/header';
-import makeNewTaskDiv from '../ui/noTasksMessage';
-import { dialogEvents } from '../events';
+// import makeNewTaskDiv from '../ui/noTasksMessage';
+// import { dialogEvents } from '../events';
+import { getFromLocalStorage } from '../localStorage';
+import addNewProjectToSidebar from '../ui/addNewProjectToSidebar';
 
-export default function loadProjectPage() {
-  createHeader();
-  //load projects from Local storage
-  //else
-  makeNewTaskDiv('projects');
-  dialogEvents();
+// export default function loadProjectPage() {
+//   // createHeader();
+//   // // if false we have no existing projects so make a task container so user can add new projects
+//   // const projects = localStorage.getItem('project');
+//   // if (!projects) {
+//   //   makeNewTaskDiv('projects');
+//   //   dialogEvents();
+//   // }
+// }
+
+// function is ran on page load
+export function addExistingProjectsToSidebar() {
+  // we also want to check if there is any projects on page load in LS to show
+  // under projects tab on page load aswell.
+  const projects = getFromLocalStorage('project');
+
+  if (!projects) return;
+
+  projects.forEach((project) => {
+    addNewProjectToSidebar(project.taskTitle);
+  });
 }
