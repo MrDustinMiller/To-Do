349842a0e42957b020761660177b770133faa296
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-10 22:51:34 -0500

    add storage/display to note page

--- src/pages/notes.js
+++ src/pages/notes.js
@@ -1,11 +1,28 @@
 import createHeader from '../ui/header';
 import makeNewTaskDiv from '../ui/noTasksMessage';
 import { dialogEvents } from '../events';
+import { getFromLocalStorage } from '../localStorage';
+import addTaskToPage from '../ui/addTaskToPage';
 
 export default function loadNotesPage() {
   createHeader();
-  //load notes from Local storage
-  //else
-  makeNewTaskDiv('notes');
-  dialogEvents();
+  const notes = localStorage.getItem('notes');
+
+  // if false we have no existing notes so make a task container so user can make some new notes
+  if (!notes) {
+    makeNewTaskDiv('notes');
+    dialogEvents();
+  } else {
+    addExistingNotesToPage('notes');
+    dialogEvents();
+  }
+}
+
+function addExistingNotesToPage(type) {
+  // get all of the users notes from LS
+  const notes = getFromLocalStorage('notes');
+
+  notes.forEach((note) => {
+    addTaskToPage(type, note);
+  });
 }
