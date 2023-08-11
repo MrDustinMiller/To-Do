514011ec84dfb6b364c84f6137966e7f1559486d
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-10 22:53:06 -0500

    finish add all tasks function

--- src/tasks/addTasks.js
+++ src/tasks/addTasks.js
@@ -1,5 +1,11 @@
-// import { changeNoTaskContainer, createNewDialogModal } from '../ui/dom';
+import { setToLocalStorage } from '../localStorage';
+import addTaskToPage from '../ui/addTaskToPage';
 
-// export default function addNewTask() {
-//   createNewDialogModal();
-// }
+export default function addToDo(type, task) {
+  setToLocalStorage(type, task);
+
+  // we dont want projects being added like tasks/notes will be
+  if (type !== 'project') {
+    addTaskToPage(type, task);
+  }
+}
