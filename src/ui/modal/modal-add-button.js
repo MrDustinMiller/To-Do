9a2784371747445de9014d6a3a7ed687d1d65a70
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-10 22:55:07 -0500

    update button class name

--- src/ui/modal/modal-add-button.js
+++ src/ui/modal/modal-add-button.js
@@ -1,7 +1,7 @@
 export default function buildModalAddTaskButton(element, className, btnText) {
   const addNewBtnModalPage = document.createElement('button');
   addNewBtnModalPage.classList.add(`${className}`);
-  addNewBtnModalPage.classList.add('modal-btn');
+  addNewBtnModalPage.classList.add('modal-add-btn');
   addNewBtnModalPage.textContent = `${btnText}`;
   element.appendChild(addNewBtnModalPage);
 }
