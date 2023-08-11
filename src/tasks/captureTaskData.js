0eddc98aafcbf14ff72054c9b4e07b1aa19272e2
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-10 22:53:16 -0500

    added funciton to capture user input

---
+++ src/tasks/captureTaskData.js
@@ -0,0 +1,57 @@
+// factory functions to make our todo object with key/value pairs assigned
+function captureModalTaskPageData() {
+  const taskTitle = captureModalTitleTextData('.modal-task-title-area');
+  const taskDetails = captureModalDetailsTextData('.modal-task-details-area');
+  const taskDate = captureModalDateData();
+  const priorityLevel = captureModalPriorityLevel();
+  return { taskTitle, taskDetails, taskDate, priorityLevel };
+}
+
+function captureModalProjectPageData() {
+  const taskTitle = captureModalTitleTextData('.modal-project-text-area');
+  return { taskTitle };
+}
+
+function captureModalNotePageData() {
+  const taskTitle = captureModalTitleTextData('.modal-notes-text-area');
+  const taskDetails = captureModalDetailsTextData('.modal-notes-details-area');
+  const taskDate = captureModalDateData();
+  return { taskTitle, taskDetails, taskDate };
+}
+
+// capture all user inputs on modal
+// only need one funciton here below, will refactor later
+function captureModalTitleTextData(elementClassName) {
+  const titleTextArea = document.querySelector(`${elementClassName}`);
+  const textData = titleTextArea.value;
+  return textData;
+}
+
+function captureModalDetailsTextData(elementClassName) {
+  const detailsTextArea = document.querySelector(`${elementClassName}`);
+  const textData = detailsTextArea.value;
+  return textData;
+}
+
+function captureModalDateData() {
+  const dateInput = document.querySelector('.modal-date-input');
+  const dateData = dateInput.value;
+  return dateData;
+}
+
+function captureModalPriorityLevel() {
+  const priorityButtons = document.querySelectorAll('.modal-btn');
+  const buttonArray = Array.from(priorityButtons);
+
+  const priorityClicked = buttonArray.find(
+    (element) => element.style.color === 'white'
+  );
+
+  return priorityClicked.textContent;
+}
+
+export {
+  captureModalTaskPageData,
+  captureModalProjectPageData,
+  captureModalNotePageData,
+};
