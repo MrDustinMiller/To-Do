347b4df4b21e9e44bd748d23e14708ffd82616ac
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-10 22:54:08 -0500

    finish ui work to add task/notes to display

---
+++ src/ui/addTaskToPage.js
@@ -0,0 +1,92 @@
+export default function addTaskToPage(type, task) {
+  if (type !== 'notes') {
+    // type would equal 'toDo'
+    addToDoToPage(task);
+  } else {
+    addNotesToPage(task);
+  }
+}
+
+function addToDoToPage(task) {
+  addTaskContainer('to-do-container');
+  const toDoContainer = document.querySelector('.to-do-container');
+
+  const toDoDiv = document.createElement('div');
+  toDoDiv.classList.add('to-do-div');
+  toDoContainer.appendChild(toDoDiv);
+
+  const toDoDivLeft = document.createElement('div');
+  toDoDivLeft.classList.add('to-do-div-left');
+  toDoDiv.appendChild(toDoDivLeft);
+
+  const checkBox = document.createElement('input');
+  checkBox.setAttribute('type', 'checkbox');
+  checkBox.classList.add('to-do-checkbox');
+  toDoDivLeft.appendChild(checkBox);
+
+  const toDo = document.createElement('p');
+  toDo.classList.add('to-do');
+  toDo.textContent = `${task.taskTitle}`;
+  toDoDivLeft.appendChild(toDo);
+
+  const toDoDivRight = document.createElement('div');
+  toDoDivRight.classList.add('to-do-div-right');
+  toDoDiv.appendChild(toDoDivRight);
+
+  const toDoDate = document.createElement('p');
+  toDoDate.classList.add('to-do-date');
+  toDoDate.textContent = `${task.taskDate}`;
+  toDoDivRight.appendChild(toDoDate);
+
+  const deleteTaskBtn = document.createElement('i');
+  deleteTaskBtn.classList.add('to-do-delete-btn');
+  deleteTaskBtn.setAttribute('class', 'fa-solid fa-trash');
+  toDoDivRight.appendChild(deleteTaskBtn);
+
+  const toDoPriorityLevel = document.createElement('p');
+  toDoPriorityLevel.classList.add('to-do-priority-level');
+  toDoPriorityLevel.textContent = `${task.priorityLevel}`;
+  toDoDivRight.appendChild(toDoPriorityLevel);
+}
+
+function addTaskContainer(className) {
+  // if container already exists just return so we dont get duplicates
+  if (document.querySelector(`.${className}`)) return;
+
+  const main = document.querySelector('.main-right');
+  const toDoContainer = document.createElement('div');
+  toDoContainer.classList.add(`${className}`);
+  main.appendChild(toDoContainer);
+}
+
+function addNotesToPage(task) {
+  addTaskContainer('notes-container');
+  const notesContainer = document.querySelector('.notes-container');
+
+  const noteDiv = document.createElement('div');
+  noteDiv.classList.add('note-div');
+  notesContainer.appendChild(noteDiv);
+
+  const noteHeaderDiv = document.createElement('div');
+  noteHeaderDiv.classList.add('note-header');
+  noteDiv.appendChild(noteHeaderDiv);
+
+  const noteHeader = document.createElement('p');
+  noteHeader.textContent = `${task.taskTitle}`;
+  noteHeaderDiv.appendChild(noteHeader);
+
+  const deleteNoteBtn = document.createElement('i');
+  deleteNoteBtn.setAttribute('class', 'fa-solid fa-x');
+  deleteNoteBtn.classList.add('note-close-btn');
+  noteHeaderDiv.appendChild(deleteNoteBtn);
+
+  const noteTextContent = document.createElement('p');
+  noteTextContent.classList.add('note-text-content');
+  noteTextContent.textContent = `${task.taskDetails}`;
+  noteDiv.appendChild(noteTextContent);
+
+  const noteDate = document.createElement('p');
+  noteDate.classList.add('note-date');
+  noteDate.textContent = `${task.taskDate}`;
+  noteDiv.appendChild(noteDate);
+}
