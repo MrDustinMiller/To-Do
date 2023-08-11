7d44153d4be87cdde2704a3ece65ce36904ff601
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-10 22:57:12 -0500

    refactor code

--- src/ui/modal/modal-task-page.js
+++ src/ui/modal/modal-task-page.js
@@ -1,37 +1,46 @@
 import { wipeModalContent } from '../dom';
 import buildModalDueDateDiv from './due-date';
 import buildModalTextArea from './text-area';
+import buildModalAddTaskButton from './modal-add-button';
+import modalTaskPageEvents from './modal-task-page-events';
 
 export default function loadModalTaskPage() {
   // if it exists then dont build the page because its open already, just return
   if (document.querySelector('.modal-task-title-area')) return;
 
-  const newContentDiv = document.querySelector('.modal-new-content-div');
-  wipeModalContent(newContentDiv);
+  const modalMainContent = document.querySelector('.modal-new-content-div');
+  wipeModalContent(modalMainContent);
   buildModalTextArea(
     'modal-task-title-area',
     'Title: Pay Bills',
-    newContentDiv
+    modalMainContent
   );
   buildModalTextArea(
-    'modal-task-title-area',
+    'modal-task-details-area',
     'Details: phone bill',
-    newContentDiv
+    modalMainContent
   );
-  buildModalDueDateDiv(newContentDiv);
-  buildModalTaskpriorityDiv(newContentDiv);
+  buildModalDueDateDiv(modalMainContent);
+  buildModalTaskpriorityDiv(modalMainContent);
+  // call specific modal task page events
+  modalTaskPageEvents();
 }
 
-function buildModalTaskpriorityDiv(newContentDiv) {
+function buildModalTaskpriorityDiv(modalMainContent) {
   const priorityDiv = document.createElement('div');
   priorityDiv.classList.add('modal-priority-div');
   priorityDiv.textContent = 'Priority: ';
-  newContentDiv.appendChild(priorityDiv);
+  modalMainContent.appendChild(priorityDiv);
 
   const prioritybuttonDiv = document.createElement('div');
   prioritybuttonDiv.classList.add('priority-btn-div');
   priorityDiv.appendChild(prioritybuttonDiv);
 
+  buildPriorityButtons(prioritybuttonDiv);
+  buildModalAddTaskButton(priorityDiv, 'modal-add-task-btn', 'Add to do');
+}
+
+function buildPriorityButtons(prioritybuttonDiv) {
   const priorityLowBtn = document.createElement('button');
   priorityLowBtn.classList.add('priority-low-btn');
   priorityLowBtn.classList.add('modal-btn');
@@ -49,10 +58,4 @@ function buildModalTaskpriorityDiv(newContentDiv) {
   priorityHighBtn.classList.add('modal-btn');
   priorityHighBtn.textContent = 'HIGH';
   prioritybuttonDiv.appendChild(priorityHighBtn);
-
-  const addTaskButton = document.createElement('button');
-  addTaskButton.classList.add('modal-add-task-btn');
-  addTaskButton.classList.add('modal-btn');
-  addTaskButton.textContent = 'Add to do';
-  priorityDiv.appendChild(addTaskButton);
 }
