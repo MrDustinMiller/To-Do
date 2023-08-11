ed54d515106a48cc3a88fc4ff3453d395f2cd1dc
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-10 22:52:13 -0500

    added storage/display to task page

--- src/pages/taskList.js
+++ src/pages/taskList.js
@@ -1,12 +1,29 @@
 import makeNewTaskDiv from '../ui/noTasksMessage';
 import createHeader from '../ui/header';
 import { dialogEvents } from '../events';
+import addTaskToPage from '../ui/addTaskToPage';
+import { getFromLocalStorage } from '../localStorage';
 
+// function is ran on page load
 export default function loadTaskList() {
   createHeader();
-  //if no tasks in localstorage show
-  makeNewTaskDiv('tasks');
-  dialogEvents();
-  // else
-  // show tasks in local storage
+  const toDo = getFromLocalStorage('toDo');
+
+  // if false we have no existing to-do's so make a task container so user can make some new tasks
+  if (!toDo) {
+    makeNewTaskDiv('tasks');
+    dialogEvents();
+  } else {
+    addExistingToDoToPage('todo');
+    dialogEvents();
+  }
+}
+
+function addExistingToDoToPage(type) {
+  // get all of the users tasks from LS
+  const toDo = getFromLocalStorage('toDo');
+
+  toDo.forEach((task) => {
+    addTaskToPage(type, task);
+  });
 }
