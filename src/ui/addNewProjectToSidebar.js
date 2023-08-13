90ed0a3ff8069999f2cdadbfc394cff85acfb690
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-12 21:04:05 -0500

    added support to add individual projects

--- src/ui/addNewProjectToSidebar.js
+++ src/ui/addNewProjectToSidebar.js
@@ -2,8 +2,20 @@ export default function addNewProjectToSidebar(projectTitle) {
   const projectSidebarLink = document.querySelector('.sidebar-project-link');
 
   const newProjectLink = document.createElement('button');
-  newProjectLink.setAttribute('class', `project-child ${projectTitle}`);
+  newProjectLink.setAttribute('class', `${projectTitle} project-child`);
+  // add link item class name so we can reuse other modules for active link clicked
+  newProjectLink.classList.add('link-item');
   newProjectLink.textContent = `${projectTitle}`;
   // insert new user project button AFTER sidebar project link
   projectSidebarLink.insertAdjacentElement('afterend', newProjectLink);
+
+  const projectsIcon = document.createElement('i');
+  projectsIcon.setAttribute('class', 'fa-solid fa-clipboard-list');
+  newProjectLink.insertAdjacentElement('afterbegin', projectsIcon);
+
+  // if project title is > 15 characters and a title that will show on hover
+  const projectTitleArray = projectTitle.split('');
+  if (projectTitleArray.length > 15) {
+    newProjectLink.setAttribute('title', `${projectTitle}`);
+  }
 }
