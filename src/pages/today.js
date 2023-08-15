08a78283a85f3062e0db27e149f73e79b5edf427
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-15 01:54:45 -0500

    add ability to show all tasks (working on it)

--- src/pages/today.js
+++ src/pages/today.js
@@ -1,11 +1,32 @@
 import createHeader from '../ui/header';
 import makeNewTaskDiv from '../ui/noTasksMessage';
 import { dialogEvents } from '../events';
+import { getFromLocalStorage } from '../localStorage';
+import addTaskToPage from '../ui/addTaskToPage';
 
 export default function loadTodayPage() {
   createHeader();
-  //load all projects/tasks from Local storage on click
-  //else
-  makeNewTaskDiv('items saved', 'task-container');
-  dialogEvents();
+  const data = localStorage.getItem('notes') || localStorage.getItem('tasks');
+
+  if (!data) {
+    makeNewTaskDiv('items saved', 'task-container');
+    dialogEvents();
+  } else {
+    addExistingDataToTodayPage('notes', 'toDo');
+    dialogEvents();
+  }
+}
+
+function addExistingDataToTodayPage(typeOne, typeTwo) {
+  //get all of the users notes/tasks from LS
+  const notes = getFromLocalStorage('notes');
+  const tasks = getFromLocalStorage('toDo');
+
+  notes.forEach((note) => {
+    addTaskToPage(typeOne, note);
+  });
+
+  tasks.forEach((task) => {
+    addTaskToPage(typeTwo, task);
+  });
 }
