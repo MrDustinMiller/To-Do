390090bcd9eb47c03f37930997d84d10937fdcac
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-15 01:53:42 -0500

    add ability to add tasks to projects

--- src/tasks/addTasks.js
+++ src/tasks/addTasks.js
@@ -1,11 +1,20 @@
 import { setToLocalStorage } from '../localStorage';
 import addTaskToPage from '../ui/addTaskToPage';
 
-export default function addToDo(type, task) {
-  setToLocalStorage(type, task);
-
+export default function addToDo(type, task, pageHeaderText) {
   // we dont want projects being added like tasks/notes will be
-  if (type !== 'project') {
+  // refactor later
+  if (type === 'notes' || type === 'toDo') {
+    setToLocalStorage(type, task);
     addTaskToPage(type, task);
+  }
+
+  if (type === 'project') {
+    setToLocalStorage(type, task);
+  }
+
+  if (type === 'project-task') {
+    setToLocalStorage(type, task, pageHeaderText);
+    addTaskToPage(type, task, 'project-container');
   }
 }
