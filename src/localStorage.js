0683a8dec730ecf2b5cfa8e2796f0c321e29d810
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-15 01:56:16 -0500

    add project page title to tasks if for a project

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,19 +1,31 @@
-function setToLocalStorage(type, task) {
+function setToLocalStorage(type, task, pageHeaderText) {
   const data = localStorage.getItem(`${type}`);
 
   //if no data
   if (!data) {
-    // populate array with our task object
-    const dataArray = [task];
-    localStorage.setItem(`${type}`, JSON.stringify(dataArray));
+    // if no value in variable then we are not adding a task to a project
+    if (!pageHeaderText) {
+      // populate array with our task object
+      const dataArray = [task];
+      localStorage.setItem(`${type}`, JSON.stringify(dataArray));
+    } else {
+      // add project page title to task
+      task.projectPage = pageHeaderText;
+      // populate array with our task object and individual project page title
+      const dataArray = [task];
+      localStorage.setItem(`${type}`, JSON.stringify(dataArray));
+    }
   } else {
+    console.log(pageHeaderText);
+    task.projectPage = pageHeaderText;
+
     // populate array with existing tasks of 'type'
     const dataArray = JSON.parse(localStorage.getItem(`${type}`));
 
     // push our new task onto end of array
     dataArray.push(task);
 
-    // set new array will all tasks old and new to local storage
+    // set new array with all tasks old and new to local storage
     localStorage.setItem(`${type}`, JSON.stringify(dataArray));
   }
 }
