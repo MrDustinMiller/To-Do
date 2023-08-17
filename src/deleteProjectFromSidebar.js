480f48573f409f976adec21ea5cee8dab7e6c69d
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-17 18:22:49 -0500

    add function to delete project from sidebar

---
+++ src/deleteProjectFromSidebar.js
@@ -0,0 +1,20 @@
+import { deleteTaskFromLocalStorage } from './localStorage';
+import { wipeDomContent } from './ui/dom';
+
+export default function deleteProjectFromSidebar(nameOfProject) {
+  const allSidebarLinkItems = document.querySelectorAll('.link-item');
+  // our top and bottom sidebar links are both called 'sidebar-links' so specify
+  // that we want the bottom container sidebar links.
+  const sidebar = document.querySelector(
+    '.sidebar-bottom-container'
+  ).firstChild;
+
+  allSidebarLinkItems.forEach((link) => {
+    // if link clicked classlist equal project name then thats the sidebar project we delete
+    if (link.classList[0] === nameOfProject) {
+      sidebar.removeChild(link);
+      deleteTaskFromLocalStorage('project', `${nameOfProject}`);
+      wipeDomContent();
+    }
+  });
+}
