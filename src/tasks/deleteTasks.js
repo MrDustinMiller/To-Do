0d1b03f02c7fb79dc1e3c36c81ab87e9c72cc8cb
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-23 22:09:36 -0500

    add if/else to find toDo and note titles

--- src/tasks/deleteTasks.js
+++ src/tasks/deleteTasks.js
@@ -1,11 +1,18 @@
 import { removeTaskFromDom } from '../ui/dom';
 
-export default function deleteTask(element) {
+export default function deleteTask(type, element) {
   element.addEventListener('click', (e) => {
-    const test = e.target.parentElement;
-    const task = test.parentNode;
-    // gets us the task title from task clicked
-    const taskTitle = task.classList[0];
-    removeTaskFromDom(task, taskTitle);
+    const parent = e.target.parentElement;
+    const task = parent.parentNode;
+
+    if (type === 'toDo') {
+      // gets us the task title from task clicked
+      const taskTitle = task.classList[0];
+      removeTaskFromDom(task, taskTitle);
+    } else {
+      // gets us the note title from task clicked
+      const noteTitle = task.childNodes[0].textContent;
+      removeTaskFromDom(task, noteTitle);
+    }
   });
 }
