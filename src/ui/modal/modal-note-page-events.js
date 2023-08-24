52d1493ce452f6fe3ca35580b939dd236b900924
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-23 22:07:20 -0500

    add a check for no task message

--- src/ui/modal/modal-note-page-events.js
+++ src/ui/modal/modal-note-page-events.js
@@ -1,15 +1,17 @@
 import { captureModalNotePageData } from '../../tasks/captureTaskData';
 import addToDo from '../../tasks/addTasks';
 import removeModal from './removeModal';
+import { checkForNoTaskMessage } from '../dom';
 
 export default function modalTaskNotesEvents() {
   const modalAddButton = document.querySelector('.modal-add-btn');
   const main = document.querySelector('.main-right');
-  const noTaskContainer = document.querySelector('.task-container');
+  const noTaskContainer = document.querySelector('.to-do-container');
 
   // capture todo object in task variable
   modalAddButton.addEventListener('click', () => {
     const task = captureModalNotePageData();
+    checkForNoTaskMessage();
     addToDo('notes', task);
     removeModal();
     if (noTaskContainer) {
