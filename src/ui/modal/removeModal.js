e4dab1875fce2c53f0bb4d162ec49bdc91fe0cdb
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-23 22:06:19 -0500

    remove task container query selector

--- src/ui/modal/removeModal.js
+++ src/ui/modal/removeModal.js
@@ -1,7 +1,6 @@
 export default function removeModal() {
   const newModal = document.querySelector('.new-modal');
   const container =
-    document.querySelector('.task-container') ||
     document.querySelector('.to-do-container') ||
     document.querySelector('.notes-container') ||
     document.querySelector('.project-container');
