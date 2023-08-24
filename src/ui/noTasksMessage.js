6090c7a51db44757399f79ebfe7678a1590faccc
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-23 22:07:44 -0500

    added a check to see if no task message exists

--- src/ui/noTasksMessage.js
+++ src/ui/noTasksMessage.js
@@ -1,16 +1,23 @@
+import { dialogEvents } from '../events';
+
 export default function makeNewTaskDiv(choice, containerClassName) {
   const mainRightBody = document.querySelector('.main-right');
+  const containerToCheckFor = document.querySelector(`.${containerClassName}`);
+  checkIfTaskDivExists(mainRightBody, containerToCheckFor);
+
   const noTaskDiv = document.createElement('div');
   noTaskDiv.classList.add(containerClassName);
   mainRightBody.appendChild(noTaskDiv);
 
   createNoTaskContainerText(noTaskDiv, choice);
   createNoTaskContainerButton(noTaskDiv);
+  dialogEvents();
 }
 
 function createNoTaskContainerText(noTaskDiv, choice) {
   const noTaskText = document.createElement('p');
   noTaskText.textContent = `You have no ${choice} yet, lets make some!`;
+  noTaskText.classList.add('no-task-text');
   noTaskDiv.appendChild(noTaskText);
 }
 
@@ -22,4 +29,10 @@ function createNoTaskContainerButton(noTaskDiv) {
   );
   newTaskButton.setAttribute('title', 'New');
   noTaskDiv.appendChild(newTaskButton);
+}
+
+function checkIfTaskDivExists(mainRightBody, containerToCheckFor) {
+  if (containerToCheckFor) {
+    mainRightBody.removeChild(containerToCheckFor);
+  }
 }
