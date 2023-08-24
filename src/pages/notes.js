a48d8b30f486c0278e2533617fd9177176dc3795
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-23 22:11:13 -0500

    change container name from task to to-do

--- src/pages/notes.js
+++ src/pages/notes.js
@@ -3,19 +3,29 @@ import makeNewTaskDiv from '../ui/noTasksMessage';
 import { dialogEvents } from '../events';
 import { getFromLocalStorage } from '../localStorage';
 import addTaskToPage from '../ui/addTaskToPage';
+import { deleteTaskFromLocalStorage } from '../localStorage';
+import { removeTaskFromDom } from '../ui/dom';
 
+// run when link is clicked
 export default function loadNotesPage() {
   createHeader();
   const notes = localStorage.getItem('notes');
 
   // if false we have no existing notes so make a task container so user can make some new notes
   if (!notes) {
-    makeNewTaskDiv('notes', 'task-container');
+    makeNewTaskDiv('notes', 'to-do-container');
     dialogEvents();
   } else {
     addExistingNotesToPage('notes');
     dialogEvents();
   }
+
+  // const deleteNoteBtn = document.querySelector('.note-close-btn');
+  // deleteNoteBtn.addEventListener('click', (e) => {
+  //   // gets us our note title
+  //   const noteTitle = e.target.parentElement.textContent;
+  //   removeTaskFromDom(,noteTitle)
+  // });
 }
 
 function addExistingNotesToPage(type) {
