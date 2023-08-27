c786745012373d66191b417ecd49e8aab9e1cd70
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-26 19:21:17 -0500

    add del project div in header if type is project

--- src/ui/header.js
+++ src/ui/header.js
@@ -1,10 +1,30 @@
-export default function createHeader() {
+export default function createHeader(type) {
   // using innerHTML so we can grab the icon as well as text
   const sidebarLinkClicked = document.querySelector('.active').innerHTML;
   const mainBody = document.querySelector('.main-right');
 
   const pageHeader = document.createElement('div');
   pageHeader.classList.add('page-header');
-  pageHeader.innerHTML = `${sidebarLinkClicked}`;
+  // pageHeader.innerHTML = `${sidebarLinkClicked}`;
+
+  const headerContent = document.createElement('div');
+  headerContent.classList.add('header-content');
+  headerContent.innerHTML = `${sidebarLinkClicked}`;
+  pageHeader.appendChild(headerContent);
+
+  if (type === 'project') {
+    const projectTrashDiv = document.createElement('div');
+    projectTrashDiv.classList.add('project-trash-div');
+    pageHeader.appendChild(projectTrashDiv);
+
+    const projectTrashCan = document.createElement('i');
+    projectTrashCan.setAttribute('class', 'fa-solid fa-trash-can');
+    projectTrashDiv.appendChild(projectTrashCan);
+
+    const projectTrashText = document.createElement('p');
+    projectTrashText.textContent = 'Delete Project';
+    projectTrashDiv.appendChild(projectTrashText);
+  }
+
   mainBody.appendChild(pageHeader);
 }
