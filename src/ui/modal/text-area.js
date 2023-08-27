9cefc3c5fc865be54f23e553ea3021c374dcf920
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-26 23:18:39 -0500

    limit character length

--- src/ui/modal/text-area.js
+++ src/ui/modal/text-area.js
@@ -1,5 +1,11 @@
 export default function buildModalTextArea(className, placeholder, element) {
   const textArea = document.createElement('textarea');
+
+  // limit character length on title text areas
+  if (className.includes('title')) {
+    textArea.maxLength = 30;
+  }
+
   textArea.classList.add(`${className}`);
   textArea.placeholder = `${placeholder}`;
   element.appendChild(textArea);
