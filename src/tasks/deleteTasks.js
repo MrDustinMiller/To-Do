12ccae4c9acb66c7ec512e962c8fd0f150e04f23
MrDustinMiller <dustin.miller@mscoding.org>
2023-08-29 19:28:48 -0500

    refactor function

--- src/tasks/deleteTasks.js
+++ src/tasks/deleteTasks.js
@@ -4,15 +4,7 @@ export default function deleteTask(type, element) {
   element.addEventListener('click', (e) => {
     const parent = e.target.parentElement;
     const task = parent.parentNode;
-
-    if (type === 'toDo') {
-      // gets us the task title from task clicked
-      const taskTitle = task.classList[0];
-      removeTaskFromDom(task, taskTitle);
-    } else {
-      // gets us the note title from task clicked
-      const noteTitle = task.childNodes[0].textContent;
-      removeTaskFromDom(task, noteTitle);
-    }
+    const noteTitle = task.childNodes[0].textContent;
+    removeTaskFromDom(task, noteTitle);
   });
 }
